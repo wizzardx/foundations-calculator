0e87612bf3fb99b1c6db6d5cb2635dd262a49af1
David <wizzardx@gmail.com>
2024-08-12 17:48:30 +0200

    Make tests a bit more "Obey the Testing Goat"-like
    Have two different tests for the same thing, help us to force the logic
    via triangulation.

--- src/functions.test.ts
+++ src/functions.test.ts
@@ -1,5 +1,11 @@
 import { add } from './functions';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
-})
+describe('add function', () => {
+  test('adding 1 and 2 returns 3', () => {
+    expect(add(1, 2)).toBe(3);
+  })
+
+  test('adding 5 and 6 returns 11', () => {
+    expect(add(5, 6)).toBe(11);
+  })
+});
