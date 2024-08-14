810070e71ac073d8f2a4abf948c134052beb4ea8
David <wizzardx@gmail.com>
2024-08-14 17:55:27 +0200

    Add subtract function

--- src/functions.test.ts
+++ src/functions.test.ts
@@ -1,4 +1,4 @@
-import { add } from "./functions";
+import { add, subtract } from "./functions";
 
 describe("add function", () => {
   test("adding 1 and 2 returns 3", () => {
@@ -7,5 +7,14 @@ describe("add function", () => {
 
   test("adding 5 and 6 returns 11", () => {
     expect(add(5, 6)).toBe(11);
+  });
+});
+
+describe("subtract function", () => {
+  test("subtracting 5 from 7 returns 2", () => {
+    expect(subtract(7, 5)).toBe(2);
+  });
+  test("subtracting 7 from 2 returns -5", () => {
+    expect(subtract(2, 7)).toBe(-5);
   });
 });
