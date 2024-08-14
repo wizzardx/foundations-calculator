c469179e7b64c367803f86734d307de338f21a1c
David <wizzardx@gmail.com>
2024-08-14 19:11:30 +0200

    Add "multiply" function

--- src/functions.test.ts
+++ src/functions.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract } from "./functions";
+import { add, subtract, multiply } from "./functions";
 
 describe("add function", () => {
   test("adding 1 and 2 returns 3", () => {
@@ -16,5 +16,15 @@ describe("subtract function", () => {
   });
   test("subtracting 7 from 2 returns -5", () => {
     expect(subtract(2, 7)).toBe(-5);
+  });
+});
+
+describe("multiply function", () => {
+  test("multiplying 2 by 3 returns 6", () => {
+    expect(multiply(2, 3)).toBe(6);
+  });
+
+  test("multiplying -5 by 10 returns -50", () => {
+    expect(multiply(-5, 10)).toBe(-50);
   });
 });
--- src/functions.ts
+++ src/functions.ts
@@ -31,3 +31,18 @@ export function add(a: number, b: number): number {
 export function subtract(a: number, b: number): number {
   return a - b;
 }
+
+/**
+ * Multiply one number by another.
+ *
+ * @param {number} a - The number to be multiplied.
+ * @param {number} b - The number to be multiplied by.
+ * @returns {number} - The product of a and b.
+ *
+ * @example
+ * const result = multiply(5, 3);
+ * console.log(result); // Output: 15
+ */
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
