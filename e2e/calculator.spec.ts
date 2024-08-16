05c1c8d05bcb4d099d959f66987ac3b7fb94fcc0
David <wizzardx@gmail.com>
2024-08-16 16:07:38 +0200

    Remove a part of my e2e test that's not applicable at the moment

--- e2e/calculator.spec.ts
+++ e2e/calculator.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from "@playwright/test";
 test("basic test", async ({ page }) => {
   await page.goto("http://localhost:8080/calculator.html");
 
-  // Check if the result div is rendered
-  const resultDiv = page.locator("#result");
-  await expect(resultDiv).toBeVisible();
+  // // Check if the result div is rendered
+  // const resultDiv = page.locator("#result");
+  // await expect(resultDiv).toBeVisible();
 
   // Check if the page title is correct
   await expect(page).toHaveTitle("Calculator");
